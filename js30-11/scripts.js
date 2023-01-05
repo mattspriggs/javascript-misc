8383248c630b08f127e8f6c0329d9018b135541a
Matt Spriggs <mspriggs1@cnm.edu>
2023-01-05 23:30:05 +1300

    Custom HTML5 Video player

---
+++ js30-11/scripts.js
@@ -0,0 +1,25 @@
+//Get our elements
+
+const player = document.querySelector('.player')
+const video = player.querySelector('.viewer')
+const progress = player.querySelector('.progress')
+const progressBar = player.querySelector('.progress__filled')
+const toggle = player.querySelector('.toggle')
+const skipButtons = player.querySelectorAll('[data-skip]')
+const ranges = player.querySelector('.player__slider')
+
+//Build out functions
+
+function togglePlay(){
+	// if(video.paused){
+	// 	video.play()
+	// }else{
+	// 	video.pause
+	// }
+	const method = video.paused ? 'play' : 'pause'
+	video[method]()
+}
+
+//Hook up event listeners
+
+video.addEventListener('click', togglePlay)
