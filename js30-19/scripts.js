990316b0c76aedfc10226e4c42a0e31d83eb3176
Matt Spriggs <mspriggs1@cnm.edu>
2023-01-17 09:36:54 +1300

    Adding photo capture to stream

--- js30-19/scripts.js
+++ js30-19/scripts.js
@@ -31,6 +31,13 @@ function takePhoto(){
 	//played sound
 	snap.currentTime = 0
 	snap.play()
+
+	//take the data out of the canvas
+	const data = canvas.toDataURL('image/jpeg')
+	const link = document.createElement('a')
+	link.href = data
+	link.setAttribute('download', 'handsome')
+	link.textContent = 'Download Image'
 }
 
 getVideo()
