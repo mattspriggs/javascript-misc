91b666934fe8d0f935faed2b0d65bd089c5ec404
Matt Spriggs <mspriggs1@cnm.edu>
2023-01-16 20:44:29 +1300

    Adding the ability to capture frames from the webcam

--- js30-19/scripts.js
+++ js30-19/scripts.js
@@ -8,7 +8,23 @@ function getVideo(){
 	navigator.mediaDevices.getUserMedia({video: true, audio: false})
 		.then(localMediaStream => {
 			console.log(localMediaStream)
+			video.srcObject = localMediaStream
+			video.play()
 		})
+		.catch(err => {
+			console.log(`OH NO!!!`, err)
+		})
+}
+
+function paintToCanvas (){
+	const width = video.videoWidth
+	const height = video.videoHeight
+	canvas.width = width
+	canvas.height = height
+
+	setInterval(() =>{
+		ctx.drawImage(video, 0, 0, width, height)
+	}, 16)
 }
 
 getVideo()
