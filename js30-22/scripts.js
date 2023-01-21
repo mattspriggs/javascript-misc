03a6ad40392cf82e043c73a9991221f7be326d97
Matt Spriggs <mspriggs1@cnm.edu>
2023-01-21 15:55:25 +1300

    Animated the link highlight with a transition timing in the CSS

--- js30-22/scripts.js
+++ js30-22/scripts.js
@@ -4,6 +4,9 @@ const highlight = document.createElement('span')
 highlight.classList.add('highlight')
 document.body.append(highlight)
 function highlightLink (){
-	console.log('Highlight!!!!')
+	const linkCoords = this.getBoundingClientRect()
+	highlight.style.width = `${linkCoords.width}px`
+	highlight.style.height = `${linkCoords.height}px`
+	highlight.style.transform = `translate(${linkCoords.left}px, ${linkCoords.top}px)`
 }
 triggers.forEach(a => a.addEventListener('mouseenter', highlightLink))
