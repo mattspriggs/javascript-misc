65be93e4bc1f71a4e012e5e45626fd1da4b9fe5d
Matt Spriggs <mspriggs1@cnm.edu>
2023-01-21 15:47:00 +1300

    Added event listeners to links

--- js30-22/scripts.js
+++ js30-22/scripts.js
@@ -1,1 +1,9 @@
 // 👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀
+const triggers = document.querySelectorAll('a')
+const highlight = document.createElement('span')
+highlight.classList.add('highlight')
+document.body.append(highlight)
+function highlightLink (){
+	console.log('Highlight!!!!')
+}
+triggers.forEach(a => a.addEventListener('mouseenter', highlightLink))
