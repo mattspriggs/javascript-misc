d982ebf3e0014ed135ea1402bde369175689e48a
Matt Spriggs <mspriggs1@cnm.edu>
2023-01-24 23:37:57 +1300

    Added padding to prevent the nav bar from jumping

--- js30-24/scripts.js
+++ js30-24/scripts.js
@@ -2,9 +2,11 @@ const nav = document.querySelector('#main')
 const topOfNav = nav.offsetTop
 function fixNav(){
 	if(window.scrollY >= topOfNav){
+		document.body.style.paddingTop = nav.offsetHeight + 'px'//prevents the nav from jumping when it is added to the doc
 		document.body.classList.add('fixed-nav')
 	}else{
-		document.body.classList.remove('fixed-nav')
+		document.body.style.paddingTop = 0//keeps it from jumping from the top of nav up to fix
+			document.body.classList.remove('fixed-nav')
 	}
 }
 
