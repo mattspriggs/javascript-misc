e8aa1d31ee33c0b4744a092d6c06048e8d65e80c
Matt Spriggs <mspriggs1@cnm.edu>
2023-01-25 17:15:40 +1300

    Added the once option which effectively unbinds the event listener and allows only a single click

--- js30-25/scripts.js
+++ js30-25/scripts.js
@@ -1,5 +1,5 @@
 const divs = document.querySelectorAll('div')
-
+const button = document.querySelector('button')
 function logText(e){
 	console.log(this.classList.value)//when you click on a div it will console log all the parents of that element which is bubbling
 //When you click on an element it starts at the outermost element with a capture until it reaches the element with the listener and bubbles the click up to the beginning, which in this case is the body
@@ -8,6 +8,13 @@ function logText(e){
 
 divs.forEach(div => div.addEventListener('click', logText,
 	// {capture: true}
-	{capture: false}
-))
+	{capture: false,
+		once: true//allows only single click and will bubble up on successive clicks with stop propagation and will fire all once with propagation
+	}))
 //With the {capture: true} option it will cause the event to fire on the way down, not when it bubbles up - by default it is false
+
+button.addEventListener('click', () =>{
+	console.log('CLICK!!!!!!')
+}, {
+	once: true//can only click the button once on the page - could be helpful to ensure that the user doesn't commit multiple actions and allow for things to process
+})
