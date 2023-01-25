43637d23dfdfc68e7ee5207ba39457e587ad3958
Matt Spriggs <mspriggs1@cnm.edu>
2023-01-25 17:43:45 +1300

    Added comments and created trigger events

--- js30-26/scripts.js
+++ js30-26/scripts.js
@@ -3,7 +3,9 @@ const background = document.querySelector('.dropdownBackground')
 const nav = document.querySelector('.top')
 
 function handleEnter(){
-console.log('ENTER!!!!!!!!!!!!!')
+	this.classList.add('trigger-enter')
+	//setTimeout(function(){ ... }//this will be undefined in a normal function
+	setTimeout(() => this.classList.add('trigger-enter-active'), 150)//in an arrow function it inherits this from the parent
 }
 
 function handleLeave(){
