d346061e12cbceb1b3d8293c604f5eb9c0f23240
Matt Spriggs <mspriggs1@cnm.edu>
2023-01-26 16:44:42 +1300

    Set up whack-a-mole browser game

---
+++ js30-30/scripts.js
@@ -0,0 +1,17 @@
+const holes = document.querySelectorAll('.hole');
+const scoreBoard = document.querySelector('.score');
+const moles = document.querySelectorAll('.mole');
+let lastHole
+function randTime(min, max){
+	return Math.round(Math.random() * (max - min) + min)
+}
+
+function randomHole(holes){
+	const idx = Math.floor(Math.random() * holes.length)
+	const hole = holes[idx]
+	if(hole === lastHole){
+		console.log('Ah nah that is the same one bud')
+		randomHole(holes)
+	}
+	lastHole = hole
+}
