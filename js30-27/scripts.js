2b42e4fbc705c8940ab7518f2f83d621bca70052
Matt Spriggs <mspriggs1@cnm.edu>
2023-01-26 14:44:35 +1300

    Added ability to track mouse movement pixel distances

--- js30-27/scripts.js
+++ js30-27/scripts.js
@@ -3,9 +3,11 @@ let isDown = false
 let startX
 let scrollLeft
 
-slider.addEventListener('mousedown', () =>{
+slider.addEventListener('mousedown', (e) =>{
 	isDown = true
-	slider.classList.add('active')
+	slider.classList.add('active')//adds the effect from the CSS when the mouse is down
+	startX = e.pageX - slider.offsetLeft
+	scrollLeft = slider.scrollLeft
 })
 slider.addEventListener('mouseleave', () =>{
 	isDown = false
@@ -15,8 +17,10 @@ slider.addEventListener('mouseup', () =>{
 	isDown = false
 	slider.classList.remove('active')
 })
-slider.addEventListener('mousemove', () =>{
+slider.addEventListener('mousemove', (e) =>{
 	if(!isDown) return//will only run when mouse is down
-	console.count(isDown)
-
+	e.preventDefault()
+	const x = e.pageX - slider.offsetLeft
+	const walk = x - startX
+	console.log(walk)
 })
