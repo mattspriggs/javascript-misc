92786c31210895032e9c88fea8f354aa5d0d9a8a
Matt Spriggs <mspriggs1@cnm.edu>
2023-01-26 15:53:46 +1300

    Added the ability to show the timer in the DOM

--- js30-29/scripts-START.js
+++ js30-29/scripts-START.js
@@ -1,16 +1,25 @@
 let countdown
+const timerDisplay = document.querySelector('.display__time-left')
 
 function timer(seconds){
 	const now = Date.now()
 	const then = now + seconds * 1000
+	displayTimeLeft(seconds)
 	countdown = setInterval(() => {
 		const secondsLeft = Math.round((then - Date.now()) / 1000)
 		//check to stop timer
-		if(secondsLeft <=0){
+		if(secondsLeft <0){
 			clearInterval(countdown)
 			return
 		}
 		//display timer
-		console.log(secondsLeft)
+		displayTimeLeft(secondsLeft)
 	}, 1000)
+}
+function displayTimeLeft(seconds){
+	const minutes = Math.floor(seconds /60)
+	const remainderSeconds = seconds % 60
+	const display = `${minutes}:${remainderSeconds <10 ? '0' : ''}${remainderSeconds}`
+	timerDisplay.textContent = display
+	console.log(minutes, remainderSeconds)
 }
