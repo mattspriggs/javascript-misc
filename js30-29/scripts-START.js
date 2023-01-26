2442274520a1c4d679c252b655a3a6972fb188fc
Matt Spriggs <mspriggs1@cnm.edu>
2023-01-26 16:13:03 +1300

    Added the change to AM/PM for the hours

--- js30-29/scripts-START.js
+++ js30-29/scripts-START.js
@@ -1,10 +1,12 @@
 let countdown
 const timerDisplay = document.querySelector('.display__time-left')
+const endTime = document.querySelector('.display__end-time')
 
 function timer(seconds){
 	const now = Date.now()
 	const then = now + seconds * 1000
 	displayTimeLeft(seconds)
+	displayEndTime(then)
 	countdown = setInterval(() => {
 		const secondsLeft = Math.round((then - Date.now()) / 1000)
 		//check to stop timer
@@ -21,5 +23,13 @@ function displayTimeLeft(seconds){
 	const remainderSeconds = seconds % 60
 	const display = `${minutes}:${remainderSeconds <10 ? '0' : ''}${remainderSeconds}`
 	timerDisplay.textContent = display
+	document.title = display
 	console.log(minutes, remainderSeconds)
+}
+
+function displayEndTime(timestamp){
+	const end = new Date(timestamp)
+	const hour = end.getHours()
+	const minutes = end.getMinutes()
+	endTime.textContent = `Be Back At ${hour > 12 ? hour - 12 : hour}:${minutes<10 ? '0' : ''}${minutes}`
 }
